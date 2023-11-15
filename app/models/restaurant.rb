5de3ea4556d4ab7c5498ec7ffec06ea461b60ec6
Helena Becker <helena.becker1104@gmail.com>
2023-11-15 17:00:59 +0100

    railsss

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
   CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :name, presence: true
+  validates :category, inclusion: { in: CATEGORY }
+  validates :address, presence: true
+  validates :category, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants, only: [:index, :new, :create, :show] do
+  resources :reviews, only: [:new, :create]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,4 +11,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
 end
